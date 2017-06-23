a308e003837a14ce53dca84b7f29a4b53c534d16
Adam Elnagger <aelnagger@wayfair.com>
2017-06-23 13:01:58 -0400

    WIP - Fix test.

--- tests/Language/ParserTest.php
+++ tests/Language/ParserTest.php
@@ -21,8 +21,8 @@ class ParserTest extends \PHPUnit_Framework_TestCase
    * @it parses large inputs.
    */
     public function testParsesLargeFiles() {
-      $file = fopen('giant.graphql', 'r');
-      $input = fread($file, filesize('giant.graphql'));
+      $file = fopen('tests/Language/giant.graphql', 'r');
+      $input = fread($file, filesize('tests/Language/giant.graphql'));
 
       Parser::parse($input);
 
